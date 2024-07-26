cdc7dfbaee564defa34752a73fcab1703bec047a
baudo2048 <baudo81@gmail.com>
2024-07-26 17:06:44 +0100

    restart

--- rest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$response = file_get_contents('https://katecode.b4a.app/dentro');
-
-echo $response;
-
-?>
--- testimage.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-type: image/gif");
-
-$data = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
-echo base64_decode($data);
-?>
